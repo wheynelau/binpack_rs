5aadba92efba545c2a4d1cb208aa0e7922a9c2d8
wheynelau <waynelau15045@gmail.com>
2025-05-14 10:17:37 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,189 @@
+use pyo3::prelude::*;
+use rand::prelude::*;
+use std::collections::HashMap;
+
+pub mod packing;
+
+#[derive(IntoPyObject, IntoPyObjectRef)]
+enum ReturnFormat {
+    Composer(HashMap<String, Vec<Vec<u32>>>),
+}
+enum InputFormat {
+    DictOfList(HashMap<String, Vec<Sequence>>),
+    ListOfDicts(Vec<HashMap<String, Sequence>>),
+}
+// Sequence usually refers to things like input_ids, position_ids, etc.
+
+type Sequence = Vec<u32>;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn fast_pack(
+    examples: HashMap<String, Vec<Sequence>>,
+    target_pack_size: usize,
+    packing_algorithm: String,
+    pad_id: Option<u32>,
+) -> PyResult<ReturnFormat> {
+    let (sequences, seq_lens) = create_hist(examples.clone(), target_pack_size);
+    let packing_algorithm =
+        packing::PackingAlgo::from_str(&packing_algorithm).expect("Invalid packing algorithm");
+    let assignments = create_packing_strategy(seq_lens, target_pack_size, packing_algorithm);
+    let result = fill_packing_strategy(assignments, sequences, target_pack_size, pad_id);
+    Ok(result)
+}
+
+fn create_hist(
+    dataset: HashMap<String, Vec<Sequence>>,
+    truncate_seq_len: usize,
+) -> (HashMap<usize, Vec<HashMap<String, Sequence>>>, Vec<usize>) {
+    let mut sequences: HashMap<usize, Vec<HashMap<String, Sequence>>> = HashMap::new();
+    let mut counts = vec![0u32; truncate_seq_len + 1];
+    let mut seq_lens: Vec<usize> = Vec::new();
+
+    // format the input data into a list of dicts
+    let dataset = dataset
+        .into_iter()
+        .flat_map(|(key, value)| {
+            value.into_iter().map(move |v| {
+                let mut entry = HashMap::new();
+                entry.insert(key.clone(), v);
+                entry
+            })
+        })
+        .collect::<Vec<_>>();
+
+    dataset.into_iter().for_each(|entry| {
+        // Only need input_ids key
+        let seq = entry
+            .get("input_ids")
+            .expect("Expected key 'input_ids' in the dataset entry");
+        let seq_len = seq.len();
+        sequences
+            .entry(seq_len)
+            .or_insert_with(Vec::new)
+            .push(entry);
+        counts[seq_len] += 1;
+    });
+
+    for seq_len in 0..(truncate_seq_len + 1) {
+        let seq_len = sequences.get(&seq_len).map_or(0, |v| v.len());
+        seq_lens.push(seq_len);
+    }
+
+    (sequences, seq_lens)
+}
+
+fn create_packing_strategy(
+    histogram: Vec<usize>,
+    pack_size: usize,
+    packing_algorithm: packing::PackingAlgo,
+) -> Vec<Vec<usize>> {
+    // this replicates the behavior of the original code
+    // all_seq_lens = []
+    // for i, count in enumerate(histogram):
+    // all_seq_lens.extend([i] * count)
+    let all_seq_lens: Vec<usize> = histogram
+        .iter()
+        .enumerate()
+        .flat_map(|(i, &count)| std::iter::repeat(i).take(count))
+        .collect();
+
+    let assignments: Vec<Vec<usize>> = packing_algorithm.pack(all_seq_lens, pack_size);
+    // let packed_seq_lens: Vec<usize> = assignments
+    //     .iter()
+    //     .map(|bin| bin.iter().sum())
+    //     .collect();
+
+    // let packing_factor =
+    assignments
+}
+
+fn fill_packing_strategy(
+    assignments: Vec<Vec<usize>>,
+    sequences: HashMap<usize, Vec<HashMap<String, Sequence>>>,
+    pack_size: usize,
+    pad_id: Option<u32>,
+) -> ReturnFormat {
+    let mut ifile_handles: HashMap<usize, (Vec<Sequence>, Vec<Sequence>)> = HashMap::new();
+    let mut rng = rand::rng();
+    for seq_len in 0..(pack_size + 1) {
+        // Try to replicate python behavior
+        let per_seq_data = sequences.get(&seq_len);
+        let per_seq_len = per_seq_data.map_or(0, |v| v.len());
+        if per_seq_len > 0 {
+            let mut input_ids = per_seq_data
+                .unwrap() // can be safely unwrapped, since we checked above
+                .iter()
+                .map(|entry| {
+                    entry
+                        .get("input_ids")
+                        .expect("Expected key 'input_ids' in the dataset entry")
+                        .clone()
+                })
+                .collect::<Vec<Sequence>>();
+            // shuffle the input_ids
+            input_ids.shuffle(&mut rng);
+
+            let positions_ids = input_ids
+                .iter()
+                .map(|seq| {
+                    let mut pos = vec![0u32; seq.len()];
+                    for i in 0..seq.len() {
+                        pos[i] = seq[i];
+                    }
+                    pos
+                })
+                .collect::<Vec<Sequence>>();
+
+            ifile_handles.insert(seq_len, (input_ids, positions_ids));
+        }
+    }
+
+    let mut input_ids = HashMap::new();
+    let mut positions_ids = HashMap::new();
+
+    for (oindex, assignment) in assignments.iter().enumerate() {
+        let mut _input_ids: Sequence = Vec::new();
+        let mut _positions_ids: Sequence = Vec::new();
+        for seq_len in assignment {
+            if let Some((input_ids_vec, positions_ids_vec)) = ifile_handles.get_mut(&seq_len) {
+                _input_ids.extend(
+                    input_ids_vec
+                        .pop()
+                        .expect("Expected input_ids to be available"),
+                );
+                _positions_ids.extend(
+                    positions_ids_vec
+                        .pop()
+                        .expect("Expected positions_ids to be available"),
+                );
+            }
+        }
+
+        // Handle padding and truncation here
+        if _input_ids.len() > pack_size {
+            _input_ids.truncate(pack_size);
+            _positions_ids.truncate(pack_size);
+        } else if let Some(pad_id) = pad_id {
+            let pad_len = pack_size - _input_ids.len();
+            _input_ids.extend(vec![pad_id; pad_len]);
+            _positions_ids.extend(vec![0; pad_len]); // position ids are all 0
+        }
+        input_ids.insert(oindex, _input_ids);
+        positions_ids.insert(oindex, _positions_ids);
+    }
+    // Here handle the conversion to the desired format
+    // for now is only composer format, which is a vec
+    let list_input_ids: Vec<Sequence> = input_ids.iter().map(|(_, v)| v.clone()).collect();
+    let _list_positions_ids: Vec<Sequence> = positions_ids.iter().map(|(_, v)| v.clone()).collect();
+    let mut result = HashMap::new();
+    result.insert("tokens".to_string(), list_input_ids);
+    ReturnFormat::Composer(result)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn binpack_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(fast_pack, m)?)?;
+    Ok(())
+}
---
+++ src/packing.rs
@@ -0,0 +1,56 @@
+use rand::prelude::*;
+pub enum PackingAlgo {
+    FirstFit,
+    FirstFitShuffle,
+    FirstFitDecreasing,
+}
+
+impl PackingAlgo {
+    pub fn pack(&self, seqlens: Vec<usize>, pack_size: usize) -> Vec<Vec<usize>> {
+        match self {
+            PackingAlgo::FirstFit => first_fit(seqlens, pack_size),
+            PackingAlgo::FirstFitShuffle => first_fit_shuffle(seqlens, pack_size),
+            PackingAlgo::FirstFitDecreasing => first_fit_decreasing(seqlens, pack_size),
+        }
+    }
+    pub fn from_str(s: &str) -> Option<PackingAlgo> {
+        match s {
+            "first_fit" => Some(PackingAlgo::FirstFit),
+            "first_fit_shuffle" => Some(PackingAlgo::FirstFitShuffle),
+            "first_fit_decreasing" => Some(PackingAlgo::FirstFitDecreasing),
+            _ => None,
+        }
+    }
+}
+
+fn first_fit(seqlens: Vec<usize>, pack_size: usize) -> Vec<Vec<usize>> {
+    let mut res: Vec<Vec<usize>> = Vec::new(); // Holds the packed bins
+    let mut sum_of_bin: Vec<usize> = Vec::new(); // Holds the sum of each bin
+    for s in seqlens {
+        // find first bin that fits
+        for i in 0..res.len() {
+            if sum_of_bin[i] + s <= pack_size {
+                res[i].push(s);
+                sum_of_bin[i] += s;
+                break;
+            } else {
+                res.push(vec![s]);
+                sum_of_bin.push(s);
+            }
+        }
+    }
+    res
+}
+
+fn first_fit_decreasing(seqlens: Vec<usize>, pack_size: usize) -> Vec<Vec<usize>> {
+    let mut seqlens = seqlens;
+    seqlens.sort_by(|a, b| b.cmp(a));
+    first_fit(seqlens, pack_size)
+}
+
+fn first_fit_shuffle(seqlens: Vec<usize>, pack_size: usize) -> Vec<Vec<usize>> {
+    let mut seqlens = seqlens;
+    let mut rng = rand::rng();
+    seqlens.shuffle(&mut rng);
+    first_fit(seqlens, pack_size)
+}
